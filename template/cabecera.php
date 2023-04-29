916919c0f959422187f6ad54e1bc8a4ee774ce16
JPalma07 <92419346+JPalma07@users.noreply.github.com>
2023-04-29 03:07:13 -0600

    Segundo cambio

--- template/cabecera.php
+++ template/cabecera.php
@@ -11,7 +11,7 @@
      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
          <ul class="nav navbar-nav">
              <li class="nav-item">
-                 <a class="nav-link" href="#">JAC</a>
+                 <a class="nav-link" href="https://github.com/JPalma07/Proyecto_WEB.git">JAC</a>
              </li>
              <li class="nav-item">
                  <a class="nav-link" href="index.php">Inicio</a>
